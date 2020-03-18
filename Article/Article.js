726186e5a476dee6c92e90536112fe96b86ef82a
Alexey Shabginov <a.shabginov@gmail.com>
2020-03-17 18:43:36 -0700

    Implemented steps 1-3

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,26 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+// Step 1 - creating a function
+function createElement(obj) {
+  // Creating elements
+  const article = document.createElement('div');
+  const header = document.createElement('h2');
+  const date = document.createElement('p');
+  const button = document.createElement('span');
+
+  // Creating classes on the elements
+  article.classList.add('article');
+  date.classList.add('date');
+  button.classList.add('expandButton');
+
+  // Step 2 - adding event listeners
+  button.addEventListener('click', (event) => {
+    button.classList.toggle('expandButton');
+  })
+
+  // Step 3
+  return article;
+}
+
